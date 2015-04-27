0d30f1f53c451e7ecc4aed7d5781064a2757487d
mishra <tech@golivelabs.io>
2015-04-27 11:10:47 -0700

    multer

--- routes/files.js
+++ routes/files.js
@@ -1,8 +1,15 @@
 var router = require('express').Router();
+var multer = require('multer');
 // get the file parser
 
-router.get('/upload', function (req, res){
+router.use(multer({ dest: '../uploads/' }));
 
+
+
+router.post('/upload',   function (req, res){
+    console.log(req.body);
+    console.log(req.files);
+    res.status(200).json({ message: 'IMPL_101' });
 });
 
 module.exports = router;
